9386f3f8078cd516889767a16eb28ba92bc78141
Aldo Utrera <autrera@palaceresorts.com>
2021-03-01 16:20:04 -0500

    Add pwa files

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import '../styles/globals.css'
 import { Provider } from 'react-redux';
 import store from '../src/store';
+import { Head } from 'next/document'
 
 function MyApp({ Component, pageProps }) {
   return (
   	<Provider store={store}>
+      <Head>
+        <meta 
+          name='viewport' 
+          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' 
+         />
+      </Head>
 	  	<Component {...pageProps} />
   	</Provider>
 	)
