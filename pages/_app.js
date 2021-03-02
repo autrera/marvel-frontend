d0c9f10832b1f4b1e8662345a2753ec9bbf91446
Aldo Utrera <accounts@aldoutrera.com>
2021-03-01 22:46:59 -0500

    Move headers from document to app page

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,37 @@
 import '../styles/globals.css'
 import { Provider } from 'react-redux';
 import store from '../src/store';
-import { Head } from 'next/document'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
   	<Provider store={store}>
       <Head>
-        <meta 
-          name='viewport' 
-          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' 
-         />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>Marvel Universe</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
       </Head>
 	  	<Component {...pageProps} />
   	</Provider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  public async getInitialProps(ctx) {
+  static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
@@ -9,34 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <meta name='application-name' content='Marvel Universe' />
-          <meta name='apple-mobile-web-app-capable' content='yes' />
-          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-          <meta name='apple-mobile-web-app-title' content='Marvel Universe' />
-          <meta name='description' content='Browse the marvel universe in your device' />
-          <meta name='format-detection' content='telephone=no' />
-          <meta name='mobile-web-app-capable' content='yes' />
-          <meta name='msapplication-config' content='/icons/browserconfig.xml' />
-          <meta name='msapplication-TileColor' content='#2B5797' />
-          <meta name='msapplication-tap-highlight' content='no' />
-          <meta name='theme-color' content='#000000' />
-                    
-          <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
-          <link rel='manifest' href='/manifest.json' />
-          <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
-          <link rel='shortcut icon' href='/icons/favicon.ico' />
-          <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
-              
-          <meta property='og:type' content='website' />
-          <meta property='og:title' content='Marvel Universe App' />
-          <meta property='og:description' content='Browse the marvel universe in your device' />
-          <meta property='og:site_name' content='Marvel Universe' />
-          <meta property='og:url' content='https://yourdomain.com' />
-          <meta property='og:image' content='https://yourdomain.com/icons/apple-touch-icon.png' />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
